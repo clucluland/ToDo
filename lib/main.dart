5a236b89289ec07da4833b91b1f970e3e5727ac2
Tetsuya Tada <tetsuya_tada@fandmnet.com>
2022-02-21 12:50:15 +0900

    大きく編集前の下ごしらえ。 不要なWedget、コードの削除。

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_sample/main_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,24 +11,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ToTo アプリ Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('ToDo アプリ'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-            ],
+      // 16:23 あたり
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel()..getTodoList(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('ToDo アプリ'),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
+          body: const Text(
+            'You have pushed the button this many times:',
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
         ),
       ),
     );
--- lib/main_model.dart
+++ lib/main_model.dart
@@ -1,10 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_sample/todo.dart';
 
 class MainModel extends ChangeNotifier {
+  List<Todo> todoList = [];
+
   Future getTodoList() async {
-    final todos = FirebaseFirestore.instance.collection('todos');
-    final snapshot = await todos.get();
+    final snapshot =
+        await FirebaseFirestore.instance.collection('todoList').get();
     final docs = snapshot.docs;
+    final todoList = docs.map((doc) => Todo(doc)).toList();
+    this.todoList = todoList;
+    notifyListeners();
   }
 }
